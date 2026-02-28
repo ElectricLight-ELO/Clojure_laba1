e8ae3ebbe6c4d7ee245740aa30abec1c44975f8e
ElectricLight-ELO <161233435+ElectricLight-ELO@users.noreply.github.com>
2026-02-28 22:44:25 +0300

    grouping by age
    1

--- src/clojure_laba1/core.clj
+++ src/clojure_laba1/core.clj
@@ -18,12 +18,34 @@
     
     (str  name)
 )
+; --------------------
+(def users   ; глобальная переменная под имена
+  [ {:name "Иван"   :age 20}
+   {:name "Мария"  :age 25}
+   {:name "Петр"   :age 20}
+   {:name "Анна"   :age 30}
+   {:name "Олег"   :age 25}
+   {:name "Света"  :age 30}
+   {:name "Вася"  :age 30}
+   {:name "Таня"  :age 31}
+   ]  )
+
+(defn group_by_age [users]
+  (group-by :age users)
 
 (defn -main
-  "Main entry point for the application"
+  "laboratorka var5"
   [& args]
-  (println "test Laba1")
-  (println (test "Student"))
+    (println "--- Группировка по возрасту---")
   
-  (println(greet "tttest"))
+  (let 
+      [grouped (group_by_age users)]
+        (doseq [[group] grouped]
+          (println (map :name group)) ; группировка
+        )
+      )
+
+ ; (println "test Laba1")
+ ; (println (test "Student"))
+ ; (println(greet "tttest"))
   )
